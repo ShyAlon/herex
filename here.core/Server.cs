0323014e4776f9b232edd9eba6dcae6760c5e172
Shy Alon <Shy Alon>
2016-07-05 17:31:21 +0300

    initial commit

---
+++ here.core/Server.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace here.core
+{
+    /// <summary>
+    /// Encapslates server access
+    /// </summary>
+    public class Server
+    {
+        public static int Counter = 0;
+    }
+}
