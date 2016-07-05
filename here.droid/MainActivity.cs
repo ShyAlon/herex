7d76e091b7e3c5afc7d9dae738cd6f5d2765c3a3
Shy Alon <Shy Alon>
2016-07-05 21:49:36 +0300

    Notification support

--- here.droid/MainActivity.cs
+++ here.droid/MainActivity.cs
@@ -7,50 +7,85 @@ using Android.Widget;
 using Android.OS;
 using here.core;
 using Android.Provider;
+using Android.Gms.Common;
+using Android.Util;
 
 namespace here.droid
 {
     [Activity(Label = "Here", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : ListActivity
+    public class MainActivity : Activity//ListActivity
     {
+        TextView msgText;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+            SetContentView(Resource.Layout.console);
 
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.CalendarList);
+            msgText = FindViewById<TextView>(Resource.Id.msgText);
 
-            // List Calendars
-            var calendarsUri = CalendarContract.Calendars.ContentUri;
+            if (IsPlayServicesAvailable())
+            {
+                var intent = new Intent(this, typeof(RegistrationIntentService));
+                StartService(intent);
+            }
 
-            string[] calendarsProjection = {
-               CalendarContract.Calendars.InterfaceConsts.Id,
-               CalendarContract.Calendars.InterfaceConsts.CalendarDisplayName,
-               CalendarContract.Calendars.InterfaceConsts.AccountName
-            };
 
-            var cursor = ManagedQuery(calendarsUri, calendarsProjection, null, null, null);
+            //// Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.CalendarList);
 
-            string[] sourceColumns = {CalendarContract.Calendars.InterfaceConsts.CalendarDisplayName,
-                CalendarContract.Calendars.InterfaceConsts.AccountName};
+            //// List Calendars
+            //var calendarsUri = CalendarContract.Calendars.ContentUri;
 
-            int[] targetResources = { Resource.Id.calDisplayName, Resource.Id.calAccountName };
+            //string[] calendarsProjection = {
+            //   CalendarContract.Calendars.InterfaceConsts.Id,
+            //   CalendarContract.Calendars.InterfaceConsts.CalendarDisplayName,
+            //   CalendarContract.Calendars.InterfaceConsts.AccountName
+            //};
 
-            SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, Resource.Layout.CalListItem,
-                cursor, sourceColumns, targetResources);
+            //var cursor = ManagedQuery(calendarsUri, calendarsProjection, null, null, null);
 
-            ListAdapter = adapter;
+            //string[] sourceColumns = {CalendarContract.Calendars.InterfaceConsts.CalendarDisplayName,
+            //    CalendarContract.Calendars.InterfaceConsts.AccountName};
 
-            ListView.ItemClick += (sender, e) => {
-                int i = (e as Android.Widget.AdapterView.ItemClickEventArgs).Position;
+            //int[] targetResources = { Resource.Id.calDisplayName, Resource.Id.calAccountName };
 
-                cursor.MoveToPosition(i);
-                int calId = cursor.GetInt(cursor.GetColumnIndex(calendarsProjection[0]));
+            //SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, Resource.Layout.CalListItem,
+            //    cursor, sourceColumns, targetResources);
 
-                var showEvents = new Intent(this, typeof(EventListActivity));
-                showEvents.PutExtra("calId", calId);
-                StartActivity(showEvents);
-            };
+            //ListAdapter = adapter;
+
+            //ListView.ItemClick += (sender, e) => {
+            //    int i = (e as Android.Widget.AdapterView.ItemClickEventArgs).Position;
+
+            //    cursor.MoveToPosition(i);
+            //    int calId = cursor.GetInt(cursor.GetColumnIndex(calendarsProjection[0]));
+
+            //    var showEvents = new Intent(this, typeof(EventListActivity));
+            //    showEvents.PutExtra("calId", calId);
+            //    StartActivity(showEvents);
+            //};
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+                else
+                {
+                    msgText.Text = "Sorry, this device is not supported";
+                    Finish();
+                }
+                return false;
+            }
+            else
+            {
+                msgText.Text = "Google Play Services is available.";
+                return true;
+            }
         }
     }
 }
